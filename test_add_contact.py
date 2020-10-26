053298aeb8372b67844f95180cec36d222259ece
Michael Pecherskiy <40406763+MichaelPecherskiy@users.noreply.github.com>
2020-10-26 22:05:52 +0300

    commit test-contact

--- test_add_contact.py
+++ test_add_contact.py
@@ -72,20 +72,6 @@ class UntitledTestCase(unittest.TestCase):
         work="qwe", email="qwe@qa.qa", bday="26", bmonth="December", byear="1997"))
         self.logout(wd)
 
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
     def tearDown(self):
         self.wd.quit()
 
