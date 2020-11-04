7d789599828057d19ea1247c82c6a19d538eefc4
Michael Pecherskiy <40406763+MichaelPecherskiy@users.noreply.github.com>
2020-11-04 20:49:47 +0300

    удалил ненужный файл app_test.py, добавил в фикстуру self.contact = ContactHelper(self)

--- conftest.py
+++ conftest.py
@@ -1,16 +1,9 @@
 from fixture.application import Application
-from fixture.app_test import Apptest
 import pytest
 
 
 @pytest.fixture
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-@pytest.fixture
-def app(request):
-    fixture = Apptest()
     request.addfinalizer(fixture.destroy)
     return fixture
