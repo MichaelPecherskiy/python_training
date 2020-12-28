f9a34fa8c930934908fdada5de9a80c087ceac94
Michael Pecherskiy <40406763+MichaelPecherskiy@users.noreply.github.com>
2020-12-28 17:17:27 +0300

    requirements

--- test/test_modified_group.py
+++ test/test_modified_group.py
@@ -1,9 +1,9 @@
 from model.group import Group
 
 
-def test_modified_group(app):
-    old_groups = app.group.get_group_list()
+def test_modified_group(app, db):
+    old_groups = db.get_group_list()
     app.group.modified_group(Group(name="qweqwe", header="qweqwe", footer="qweqwe"))
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
 
