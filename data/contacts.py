7ef71b2381c97c8fabd7341f0f4ed048b6427511
Michael Pecherskiy <40406763+MichaelPecherskiy@users.noreply.github.com>
2020-12-29 22:04:27 +0300

    задание 28

--- data/contacts.py
+++ data/contacts.py
@@ -1,15 +1,10 @@
-# -*- coding: utf-8 -*-
 from model.contact import Contact
 
-
 testdata = [
-    Contact(firstname="1", middlename="1", lastname="1",nickname="1",
-            company="1", homephone="1", title="1", address="1", workphone="1", fax="1", email="1", mobilephone="1",
-            secondaryphone="1", email2="1", email3="1", homepage="1", address2="1", notes="1"),
-    Contact(firstname="2", middlename="2", lastname="2",nickname="2",
-            company="2", homephone="2", title="2", address="2", workphone="2", fax="2", email="2", mobilephone="2",
-            secondaryphone="2", email2="2", email3="2", homepage="2", address2="2", notes="2"),
+    Contact(firstname="fname1", lastname="lname1", address="adress1",
+            homephone="+7111111111", mobilephone="+7111111111", workphone="+7111111111", secondaryphone="+7111111111",
+            email="1mail@test.te", email2="1mail2@test.te", email3="1mail3@test.te"),
+    Contact(firstname="fname2", lastname="lname2", address="adress2",
+            homephone="+7222222222", mobilephone="+7222222222", workphone="+7222222222", secondaryphone="+7222222222",
+            email="2mail@test.te", email2="2mail2@test.te", email3="2mail3@test.te")
 ]
-
-
-
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import allure
+import pytest
 
 
-def test_untitled_test_case(app, db, json_contact, check_ui):
-    contact = json_contact
-    old_contacts = db.get_contact_list()
-    app.contact.create_contact(contact)
-    new_contacts = db.get_contact_list()
-    old_contacts.append(contact)
-    if check_ui:
-        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+def test_add_contact(app, db, json_contacts):
+    contact = json_contacts
+    with allure.step('Given a contact list'):
+        old_contact = db.get_contact_list()
+    with allure.step('When I add a user %s the list' % contact):
+        app.contact.create(contact)
+    with allure.step('Then the new user list is equal to the old list with the added user'):
+        new_contact = db.get_contact_list()
+        old_contact.append(contact)
+        assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
